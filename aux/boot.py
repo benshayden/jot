3db5f46202f51d606bb58e4a6c18e84330f4c213
Ben Hayden <benshayden@users.noreply.github.com>
2025-03-03 11:15:32 -0800

    Update boot.py

--- aux/boot.py
+++ aux/boot.py
@@ -1,1 +1,22 @@
+import storage
+import board
+import supervisor
+from neopixel import NeoPixel
 
+neopixel = NeoPixel(board.NEOPIXEL, 1)
+neopixel.fill((10, 0, 0))
+
+mnt = storage.getmount('/')
+label = '_JOT'
+if mnt.label != label:
+	storage.remount('/', readonly=False)
+	mnt = storage.getmount('/')
+	mnt.label = label
+	storage.remount('/', readonly=True)
+storage.enable_usb_drive()
+
+# todo https://pid.codes/
+# 0x62='b', 0x68='h'
+supervisor.set_usb_identification(manufacturer='bsh', product='_jot', vid=0x1209, pid=0x6268)
+
+neopixel.fill((0, 0, 0))
