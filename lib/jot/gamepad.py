c21b8a32730fd53f2098c3088b8bedcb14b80c8c
Ben Hayden <benshayden@users.noreply.github.com>
2025-03-06 18:37:42 -0800

    Update gamepad.py

--- lib/jot/gamepad.py
+++ lib/jot/gamepad.py
@@ -1,33 +1,33 @@
 import time
 from adafruit_hid import find_device
 
-REPORT_DESCRIPTOR = bytes((
-  0x05, 0x01,  # Usage Page (Generic Desktop Ctrls)
-  0x09, 0x05,  # Usage (Game Pad)
-  0xA1, 0x01,  # Collection (Application)
-  0x85, 0x04,  #   Report ID (4)
-  0x05, 0x09,  #   Usage Page (Button)
-  0x19, 0x01,  #   Usage Minimum (Button 1)
-  0x29, 0x18,  #   Usage Maximum (Button 24)
-  0x15, 0x00,  #   Logical Minimum (0)
-  0x25, 0x01,  #   Logical Maximum (1)
-  0x75, 0x01,  #   Report Size (1)
-  0x95, 0x18,  #   Report Count (24)
-  0x81, 0x02,  #   Input (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position)
-  0x05, 0x01,  #   Usage Page (Generic Desktop Ctrls)
-  0x15, 0x81,  #   Logical Minimum (-127)
-  0x25, 0x7F,  #   Logical Maximum (127)
-  0x09, 0x30,  #   Usage (X)
-  0x09, 0x31,  #   Usage (Y)
-  0x09, 0x32,  #   Usage (Z)
-  0x09, 0x35,  #   Usage (Rz)
-  0x75, 0x08,  #   Report Size (8)
-  0x95, 0x04,  #   Report Count (4)
-  0x81, 0x02,  #   Input (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position)
-  0xC0,        # End Collection
-))
-
 class Gamepad:
+	DESCRIPTOR = bytes((
+		0x05, 0x01,  # Usage Page (Generic Desktop Ctrls)
+		0x09, 0x05,  # Usage (Game Pad)
+		0xA1, 0x01,  # Collection (Application)
+		0x85, 0x04,  #   Report ID (4)
+		0x05, 0x09,  #   Usage Page (Button)
+		0x19, 0x01,  #   Usage Minimum (Button 1)
+		0x29, 0x18,  #   Usage Maximum (Button 24)
+		0x15, 0x00,  #   Logical Minimum (0)
+		0x25, 0x01,  #   Logical Maximum (1)
+		0x75, 0x01,  #   Report Size (1)
+		0x95, 0x18,  #   Report Count (24)
+		0x81, 0x02,  #   Input (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position)
+		0x05, 0x01,  #   Usage Page (Generic Desktop Ctrls)
+		0x15, 0x81,  #   Logical Minimum (-127)
+		0x25, 0x7F,  #   Logical Maximum (127)
+		0x09, 0x30,  #   Usage (X)
+		0x09, 0x31,  #   Usage (Y)
+		0x09, 0x32,  #   Usage (Z)
+		0x09, 0x35,  #   Usage (Rz)
+		0x75, 0x08,  #   Report Size (8)
+		0x95, 0x04,  #   Report Count (4)
+		0x81, 0x02,  #   Input (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position)
+		0xC0,        # End Collection
+	))
+	
 	def __init__(self, devices, joystick0, joystick1, name='gamepad', ms=50):
 		self._device = find_device(devices, usage_page=1, usage=5)
 		self._report = bytearray(7)
@@ -64,7 +64,8 @@ class Gamepad:
 			self._send()
 	
 	def _round(self, n):
-		return min(127, max(-127, round(127.0 * n / self.samples))) & 0xff
+		return min(127, max(-127, round(127.0 * n))) & 0xff
+
 	
 	def _send(self):
 		self._device.send_report(self._report)
