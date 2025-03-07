9cddc1142d83c5ad8a4b947e1c4eae8d8b80de67
Ben Hayden <benshayden@users.noreply.github.com>
2025-03-06 17:47:09 -0800

    RunMode

--- main/commands/reset.py
+++ main/commands/reset.py
@@ -1,2 +1,5 @@
 # hard reset
+if len(ARGS) > 1 and hasattr(microcontroller.RunMode, ARGS[1]):
+	# https://docs.circuitpython.org/en/latest/shared-bindings/microcontroller/#microcontroller.RunMode
+	microcontroller.on_next_reset(microcontroller.RunMode[ARGS[1]])
 microcontroller.reset()
