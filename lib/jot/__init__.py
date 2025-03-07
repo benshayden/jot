04e459131db57140d6030548073d8b892b261b06
Ben Hayden <benshayden@users.noreply.github.com>
2025-03-07 15:05:44 -0800

    import packets

--- lib/jot/__init__.py
+++ lib/jot/__init__.py
@@ -1,4 +1,5 @@
 from .gamepad import Gamepad
+from .packets import AccelerometerPacket, ButtonPacket, ColorPacket, GyroPacket, JoystickPacket, MagnetometerPacket, ProximityPacket
 from .ring_buffer import RingBuffer
 from .scripts import run_script, CommandLineInterface
 import .tasks
