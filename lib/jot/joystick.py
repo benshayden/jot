6f91653e7193a26076aa2a7237f5701ff771e9d8
Ben Hayden <benshayden@users.noreply.github.com>
2025-03-10 21:58:17 -0700

    Update joystick.py

--- lib/jot/joystick.py
+++ lib/jot/joystick.py
@@ -14,14 +14,20 @@ class JoyStick:
 		self._x = RingBuffer(capacity=window_ms, init=0.0)
 		self._y = RingBuffer(capacity=window_ms, init=0.0)
 		self.task = tasks.create(ms=1)(self.loop)
+		self._x_cache = None
+		self._y_cache = None
 
 	@property
 	def x(self) -> float:
-		return sum(self._x) / len(self._x) if len(self._x) else 0.0
+		if self._x_cache is None:
+			self._x_cache = sum(self._x) / len(self._x) if len(self._x) else 0.0
+		return self._x_cache
 
 	@property
 	def y(self) -> float:
-		return sum(self._y) / len(self._y) if len(self._y) else 0.0
+		if self._y_cache is None:
+			self._y_cache = sum(self._y) / len(self._y) if len(self._y) else 0.0
+		return self._y_cache
 
 	def _norm(self, val, neg, pos):
 		if neg.min <= val <= neg.max:
@@ -31,5 +37,6 @@ class JoyStick:
 		return 0.0
 	
 	def loop(self, now):
+		self._x_cache = self._y_cache = None
 		self._x(self._norm(self._xpin.value, self._xneg, self._xpos))
 		self._y(self._norm(self._ypin.value, self._yneg, self._ypos))
