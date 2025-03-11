cd42ad489f843e6c8d615c52c8ae98f9611d555d
Ben Hayden <benshayden@users.noreply.github.com>
2025-03-11 15:56:23 -0700

    Update layer.py

--- main/switches/layer.py
+++ main/switches/layer.py
@@ -1,1 +1,4 @@
-if event.pressed: set_layer(ARGS[1])
+if SwitchEvent.current.pressed:
+	set_layer(ARGS[1])
+	gamepad.task.enabled = (ARGS[1] == 'gamepad')
+	joymouse.task.enabled = not gamepad.task.enabled
