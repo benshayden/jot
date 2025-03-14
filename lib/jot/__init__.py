a489ea823f19c51eac6fb70551454b8e5310231b
Ben Hayden <benshayden@users.noreply.github.com>
2025-03-14 09:10:42 -0700

    DigitalIn, DigitalOut

--- lib/jot/__init__.py
+++ lib/jot/__init__.py
@@ -5,3 +5,15 @@ from .scripts import run_script, uncache_script, CommandLineInterface
 from . import tasks
 from .joystick import Interval, JoyStick
 from .switch_event import SwitchEvent, PRESS, RELEASE
+import digitalio
+
+def DigitalOut(pin):
+	dout = digitalio.DigitalInOut(pin)
+	dout.direction = digitalio.Direction.OUTPUT
+	return dout
+
+def DigitalIn(pin, *, pull_down=False):
+	din = digitalio.DigitalInOut(board.SWITCH)
+	din.direction = digitalio.Direction.INPUT
+	din.pull = digitalio.Pull.DOWN if pull_down else digitalio.Pull.UP
+	return din
