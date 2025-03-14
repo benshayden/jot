40cb6acfbaea870715a03f0cb9dc48738d213298
Ben Hayden <benshayden@users.noreply.github.com>
2025-03-14 09:46:23 -0700

    only import modules required for both main and aux

--- lib/jot/__init__.py
+++ lib/jot/__init__.py
@@ -1,10 +1,7 @@
-from .gamepad import Gamepad
 from .packets import AccelerometerPacket, ButtonPacket, ColorPacket, GyroPacket, JoystickPacket, MagnetometerPacket, Packet, ProximityPacket
 from .ring_buffer import RingBuffer
-from .scripts import run_script, uncache_script, CommandLineInterface
 from . import tasks
 from .joystick import Interval, JoyStick
-from .switch_event import SwitchEvent, PRESS, RELEASE
 import digitalio
 
 def DigitalOut(pin):
